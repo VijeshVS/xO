e3f1b20656e9d2b7e521d27c7be24382010912a5
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-10-21 16:53:13 +0530

    api-key

--- app/lib/askGemini.ts
+++ app/lib/askGemini.ts
@@ -1,6 +1,6 @@
 "use server"
 import { GoogleGenerativeAI } from "@google/generative-ai";
-const API_KEY = process.env.VITE_GEMINI_API_KEY;
+const API_KEY = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY as string);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
