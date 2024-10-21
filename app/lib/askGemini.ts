746421e1e585c634afdb80420d35f37217aea796
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-10-21 18:49:12 +0530

    gemini is dumb

--- app/lib/askGemini.ts
+++ app/lib/askGemini.ts
@@ -6,11 +6,12 @@ export const askGemini = async (grid: string, level: string, team: string,API_KE
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const prompt = `${grid} This is the current 3x3 grid for a Tic-Tac-Toe game. 
   You are playing as ${team}. Choose an optimal move based on the difficulty level ${level}. 
-  Select a random empty space ("") from the grid to place your marker ("X" or "O"), 
+  Select a random empty space ("") from the grid to place your marker, 
   ensuring you avoid any spaces where "X" or "O" is already placed. 
   Only return the index of the chosen empty space (i.e., where the value is ""). 
   Do not provide any extra explanations or output beyond the index.`
   
+  console.log(prompt)
   const result = await model.generateContent([prompt]);
 
   try {
