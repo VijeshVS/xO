b5e9f21709cde9c488c091ca213b735304179b36
Vijesh Shetty <vijeshsshetty@gmail.com>
2025-02-25 12:56:38 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,50 +1,54 @@
-import { WebSocketServer } from 'ws';
-import { Messages } from './types/main';
-import GameManager from './GameManager';
+import { WebSocketServer } from "ws";
+import { Messages } from "./types/main";
+import GameManager from "./GameManager";
 
 const wss = new WebSocketServer({ port: 8080 });
 const gameManager = new GameManager();
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
 
-  ws.on('message', function message(data) {
+  ws.on("message", function message(data) {
     const message = JSON.parse(data.toString());
 
-    if(message.type == Messages.INIT_GAME){
-        const {status, game} = gameManager.createGame(ws);
+    if (message.type == Messages.INIT_GAME) {
+      const { status, game } = gameManager.createGame(ws);
 
-        if(game == undefined){
-          ws.send(JSON.stringify({
+      if (game == undefined) {
+        ws.send(
+          JSON.stringify({
             type: Messages.INIT_GAME,
-            status: 'error',
-            message: 'Game already exists'
-          }));
-        }
-        else if(status == 'pending'){
-          ws.send(JSON.stringify({
+            status: "error",
+            message: "Some error occured",
+          })
+        );
+      } else if (status == "pending") {
+        ws.send(
+          JSON.stringify({
             type: Messages.INIT_GAME,
-            status: 'pending'
-          }));
-        }
-        else if(status == 'ready'){
-          game.player1.send(JSON.stringify({
+            status: "pending",
+          })
+        );
+      } else if (status == "ready") {
+        game.player1.send(
+          JSON.stringify({
             type: Messages.INIT_GAME,
-            status: 'ready',
-            player: 1
-          }));
+            status: "ready",
+            player: 1,
+          })
+        );
 
-          game.player2.send(JSON.stringify({
+        game.player2.send(
+          JSON.stringify({
             type: Messages.INIT_GAME,
-            status: 'ready',
-            player: 2
-          }));  
-        }
-    }
-    else if(message.type == Messages.MOVE){
-      
+            status: "ready",
+            player: 2,
+          })
+        );
+      }
+    } else if (message.type == Messages.MOVE) {
     }
   });
 
-  ws.send('something');
+  ws.send("something");
 });
